e40f691bd1e67fb6dea9b2e69187ceda6f6aec4d
viktoria1592 <92viktori@mail.ru>
2018-11-22 23:29:25 +0300

    add slider, gallery

--- app/js/script.js
+++ app/js/script.js
@@ -26,3 +26,59 @@ function initMap() {
     });
     myMap.geoObjects.add(myPlacemark);
 };
+$(document).ready(function(){
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+                if(value == "all")        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }       else        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');//
+          $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+    });
+   if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+});
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+    $(".zoom").hover(function(){
+				$(this).addClass('transition');
+	}, function(){
+		$(this).removeClass('transition');
+	});
+});
+		window.onload = function() {
+    new Swiper(".swiper-container",{
+        slidesPerView: 3,
+		spaceBetween: 30,
+        speed: 400,
+        pagination: {
+            el: ".swiper-pagination",
+			clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+			767: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+			559: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+		}
+    })
+}
+;
+
